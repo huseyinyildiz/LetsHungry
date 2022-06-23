03ff610bf2f2c489a9a1a9a2178848b8186f9b93
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2022-06-23 19:54:05 +0300

    update

--- LetsHungry.DataAccessLayer/Repository/Repository.cs
+++ LetsHungry.DataAccessLayer/Repository/Repository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace LetsHungry.DataAccessLayer.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
-    {
+    {   
         protected readonly LetsHungryDbContext _db;
         private readonly DbSet<T> _dbSet;
 
